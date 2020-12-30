c04f0d35d40134168a0d2dcea9bd91c3470e6a32
Nicolas Gachancipa <nicolasgapar@gmail.com>
2020-12-30 00:24:45 -0500

    Local commit: Minor changes to matplotlib plot.

--- ML/support_ML_functions.py
+++ ML/support_ML_functions.py
@@ -13,22 +13,10 @@ Author: Nicolas Gachancipa
 # Imports.
 from Graphing.support_graphing_functions import plot, naming
 from matplotlib import cm
-import matplotlib.colors as mcolors
+from matplotlib.colors import ListedColormap
 import numpy as np
 import os
 import pandas as pd
-
-
-# Functions.
-def resize():
-    s = 0.03
-    pos = ax.get_position()
-    smh = pos.height / 8.
-    loh = pos.height * 0.675
-    w = 0.06 * pos.width
-    x0 = pos.x1 + 0.065 * pos.width
-    cb1ax.set_position([x0, pos.y0 + loh + smh + 2 * s, w, smh])
-    cb2ax.set_position([x0, pos.y0 + loh + s, w, smh])
 
 
 def plot_scintillation_detections(file, graph_type, prn, threshold, location, signal_type_name, date, time_period=1):
@@ -52,18 +40,25 @@ def plot_scintillation_detections(file, graph_type, prn, threshold, location, si
     plt, _ = plot(x_values, y_values, None, graph_name, title, subtitle, output_dir, file_type='REDOBS',
                   graph_type=graph_type, set_y_axis_range=True, y_axis_start_value=0, y_axis_final_value=1, units=None)
 
+    # Plot colors.
+    top = cm.get_cmap('Oranges_r', 128)
+    bottom = cm.get_cmap('Blues', 32)
+    newcolors = np.vstack((top(np.linspace(0, 1, 128)),
+                           bottom(np.linspace(0, 1, 32))))
+    newcmp = ListedColormap(newcolors, name='OrangeBlue')
+
     # Add RNN detections to the plot.
     ax = plt.gca()
-    new_array = [np.array([i] * 2) for i in detections]
+    new_array = [np.array([abs(i-3)] * 2) if i <= 3 else np.array([i] * 2) for i in detections]
     new_array = np.stack(new_array, axis=0).T
-    detection_map = ax.matshow(new_array, cmap='Oranges', origin='lower', alpha=0.3,
+    detection_map = ax.matshow(new_array, cmap=newcmp, origin='lower', alpha=0.3,
                                extent=[x_values[0], x_values[-1], 0, 1], aspect='auto', vmin=0, vmax=4.5)
 
     # Plot visual parameters.
     ax.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True, labeltop=False)
     cbar = plt.colorbar(detection_map, ticks=[0, 1, 2, 3, 4])
-    cbar.ax.set_yticklabels(['No Scintillation', 'Low', 'High', 'Extreme', 'Multi-Path'])
+    cbar.ax.set_yticklabels(['Extreme', 'High', 'Low', 'No Scintillation', 'Multi-Path'])
     plt.subplots_adjust(top=0.85, bottom=0.1)
 
     # Return plot.
